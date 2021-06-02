76322ee6a010e8a77db9e2be2dce62f067853877
Dayvson Silva <dayvson.silva@squadra.com.br>
2021-06-01 23:52:37 -0300

    Ajustes config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,3 @@
-window.REACT_APP_API_URL = 'https://localhost:44321/api/';
+//window.REACT_APP_API_URL = 'https://localhost:44321/api/';
+window.REACT_APP_API_URL = 'https://mercadoapi20210601234318.azurewebsites.net/api';
 window.REACT_APP_ENVIRONMENT = 'LOCAL';
